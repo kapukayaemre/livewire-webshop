3ea7632a1658659ba4f40297fbced0a9e49549c1
Emre Kapukaya <kapukayaemre@hotmail.com>
2024-08-30 15:58:19 +0300

    database seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Image;
+use App\Models\Product;
+use App\Models\ProductVariant;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +17,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Product::factory(4)
+            ->hasVariants(5)
+            ->has(Image::factory()->count(3))
+            ->afterCreating(function (Product $product) {
+                $product->images->first()->update(['featured' => true]);
+            })
+            ->create();
+
         // User::factory(10)->create();
 
-        User::factory()->create([
+        /*User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]);*/
     }
 }
