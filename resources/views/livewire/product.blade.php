a2cdc54bcf18bf89dddbd9f86b46ec84b2fd35a6
Emre Kapukaya <kapukayaemre@hotmail.com>
2024-09-01 17:23:56 +0300

    add to cart validation completed

--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -1,13 +1,13 @@
 <div class="grid grid-cols-2 gap-10 py-12">
-    <div class="space-y-4">
+    <div class="space-y-4" x-data="{ image: '{{ $this->getProduct->image->path }}' }">
         <div class="bg-white p-5 rounded-lg shadow">
-            <img src="{{ $this->getProduct->image->path }}" alt="">
+            <img x-bind:src="image" alt="">
         </div>
 
         <div class="grid grid-cols-4 gap-4">
             @foreach($this->getProduct->images as $image)
-                <div class="rounded bg-white p-2 shadow">
-                    <img src="{{ $image->path }}" alt="">
+                <div class="rounded bg-white p-2 shadow cursor-pointer">
+                    <img src="{{ $image->path }}" @click="image = '{{ $image->path }}' " alt="">
                 </div>
             @endforeach
         </div>
@@ -24,13 +24,18 @@
         </div>
 
         <div class="mt-4 space-y-4">
-            <select class="block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-800">
-            @foreach($this->getProduct->variants as $variant)
-                <option value="{{ $variant->id }}">{{ $variant->size }} / {{ ucfirst($variant->color) }}</option>
-            @endforeach
+            <select wire:model="variant" class="block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-800">
+                <option selected>Select Variant</option>
+                @foreach($this->getProduct->variants as $variant)
+                    <option value="{{ $variant->id }}">{{ $variant->size }} / {{ ucfirst($variant->color) }}</option>
+                @endforeach
             </select>
 
-            <x-button>Add to Cart</x-button>
+            @error('variant')
+                <div class="mt-2 text-red-600">{{ $message }}</div>
+            @enderror
+
+            <x-button wire:click="addToCart">Add to Cart</x-button>
         </div>
     </div>
 </div>
