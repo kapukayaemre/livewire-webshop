88c6a02a5fc57c0eb1f074f4610fccefb2ad9ddb
Emre Kapukaya <kapukayaemre@hotmail.com>
2024-09-01 18:45:34 +0300

    cart items stored in database

--- app/Livewire/Product.php
+++ app/Livewire/Product.php
@@ -3,11 +3,13 @@
 namespace App\Livewire;
 
 use App\Actions\Webshop\AddProductVariantToCart;
+use Laravel\Jetstream\InteractsWithBanner;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Product extends Component
 {
+    use InteractsWithBanner;
     public $productId;
     public $variant;
 
@@ -26,6 +28,8 @@ class Product extends Component
         $cart->add(
             variantId: $this->variant
         );
+
+        $this->banner('Your product has been added to your cart');
     }
 
     #[Computed]
